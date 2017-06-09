d08b73c75cdb5ab7d8ea55fb7952d88e8935c238
dpn <dpn@home>
2017-06-09 18:54:48 -0400

    add gitignore dockerignore files

--- api/controllers/grafAlertsController.js
+++ api/controllers/grafAlertsController.js
@@ -7,6 +7,13 @@ exports.send_to_centreon = function(req, res) {
    tools.call_centreon('toto','titi',function(err,result){
       console.log('The user is', result)
   })
-   res.json("niark");
+   if (!req.body) return res.sendStatus(400)
+   var message=req.body.message;
+   var ruleName=req.body.ruleName;
+   var state=req.body.state;
+   console.log('The message is ', message)
+   console.log('The message is ', ruleName)
+   console.log('The state is ', state)
+   res.json('OK');
 };
 
