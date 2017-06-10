62e5556af3cbcf160716604a1647df20680eb5fb
dpn <dpn@home>
2017-06-10 19:47:14 -0400

    gestion des erreurs + tests

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 // config1.js
 module.exports = {
   nsca_path: "/usr/lib/nsca_path",
+  states: {
+    ok: ["ok"],
+    warning: ["paused", "no_data", "pending" ],
+    critical: ["alerting"]
+  }
 }
